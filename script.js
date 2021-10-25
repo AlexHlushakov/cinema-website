d8483d3a753a1f56455518c2f0eb2e4600aae5a8
Alex Hlushakov <88965812+AlexHlushakov@users.noreply.github.com>
2021-10-26 01:15:43 +0800

    Major updates in functionality
    Added functionality in uploading changes to FireStore database and fetching available seat info from it. Managed other page objects according to new functionality.

--- script.js
+++ script.js
@@ -1,76 +1,152 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyCsuO-6sH18GVSyAdFSAspbPcdJHSuThR0",
+   authDomain: "cinema-web-9498f.firebaseapp.com",
+   projectId: "cinema-web-9498f",
+   storageBucket: "cinema-web-9498f.appspot.com",
+   messagingSenderId: "292019932133",
+   appId: "1:292019932133:web:177845f372bf9695ca7d0c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+
+window.onload = function () {
+   fillSeats();
+};
+
+
+// Initialize variables
 const bigRowBlock = document.querySelector('.big'),
    smallRowBlock = document.querySelectorAll('.small'),
    currentPrice = document.querySelector('.current-price'),
    cinema = document.querySelector('#cinema-selector'),
    time = document.querySelector('#time-selector'),
    movie = document.querySelector('.film-title'),
-   buyBtn = document.querySelector('#buy-btn');
+   buyBtn = document.querySelector('#buy-btn'),
+   dates = document.querySelectorAll('.session-date-item'),
+   headerLogo = document.querySelector('.header-logo'),
+   menu = document.querySelector('.menu');
 
 
-let bigRowCount = 76, smallRowCount = 40, ticketPrice = 400;
+let chosenSeats = [], chosenDate, chosenMonth, chosenCinema = cinema.value, chosenTime = time.value, chosenMovie = movie.textContent, ticketPrice = 400;
 
-function fillSeats() {
-   for (let i = 0; i < bigRowCount; i++) {
-      let seat = document.createElement("div");
-      seat.className = "seat-single available";
-      bigRowBlock.append(seat);
+
+// event listeners
+headerLogo.addEventListener('click', () => {
+   if (menu.className !== 'menu active-menu') {
+      menu.classList.add('active-menu')
+   } else {
+      menu.classList.remove('active-menu')
    }
-   for (let i = 0; i < smallRowBlock.length; i++) {
-      for (let j = 0; j < smallRowCount; j++) {
-         let seat = document.createElement("div")
-         seat.className = "seat-single available";
-         smallRowBlock[i].append(seat);
-      }
-   }
-}
-
-fillSeats();
-
+})
 
 cinema.addEventListener("change", () => {
    chosenCinema = cinema.value;
+   refreshSeats();
 })
 
 time.addEventListener('change', () => {
    chosenTime = time.value;
+   refreshSeats();
 })
 
-const seats = document.querySelectorAll('.seat-single'),
-   dates = document.querySelectorAll('.session-date-item');
-console.log(seats);
-let chosenSeats = [], chosenDate, chosenMonth, chosenCinema = cinema.value, chosenTime = time.value, chosenMovie = movie.textContent;
-
 for (let i = 0; i < dates.length; i++) {
-   dates[i].addEventListener('click', () => {
-      for (let date of dates) {
-         date.classList.remove('active');
-      };
-      dates[i].classList.add("active")
+   if (dates[i].className === "session-date-item active") {
       chosenDate = dates[i].childNodes[3].textContent;
       chosenMonth = dates[i].childNodes[1].textContent;
-      console.log(chosenDate, chosenMovie, chosenMonth, chosenCinema, chosenTime);
-   })
+   } else {
+      dates[i].addEventListener('click', () => {
+         for (let date of dates) {
+            date.classList.remove('active');
+         };
+         dates[i].classList.add("active");
+         chosenDate = dates[i].childNodes[3].textContent;
+         chosenMonth = dates[i].childNodes[1].textContent;
+         refreshSeats();
+      })
+   }
 }
 
 
-for (let seat of seats) {
-   console.log(seat);
-   seat.addEventListener('click', () => {
-      console.log(seat, chosenSeats);
-      if (seat.className === "seat-single available") {
-         seat.classList.remove("available")
-         seat.classList.add("chosen")
-         chosenSeats.push(seat);
+buyBtn.addEventListener('click', () => {
+   occupyingSeats(chosenSeats);
+   saveToFirestore().then(() => {
+      chosenSeats = [];
+      console.log("saving is finished");
+      updatePrice();
+   });
+})
+
+
+
+
+//functions
+
+function refreshSeats() {
+   bigRowBlock.innerHTML = '';
+   smallRowBlock[0].innerHTML = '';
+   smallRowBlock[1].innerHTML = '';
+   fillSeats();
+}
+
+
+function fillSeats() {
+   async function getSeats() {
+      const snapshot = await db.collection('movie').doc(`${chosenMovie}`).collection(`${chosenCinema}`).doc(`${chosenDate}` + `${chosenMonth}`).collection(`${chosenTime}`).get()
+      return snapshot.docs.map(doc => doc.data());
+   }
+   getSeats().then(
+      function (results) {
+         console.log(results)
+         for (let item of results) {
+            let textId = item.elemId, cl = item.class;
+            if (textId.includes('bigRowBlock_')) {
+               let seat = document.createElement("div");
+               seat.className = cl;
+               seat.id = textId;
+               bigRowBlock.append(seat);
+            } else if (textId.includes('smallRowBlock_1block')) {
+               let seat = document.createElement("div")
+               seat.className = cl;
+               seat.id = textId;
+               smallRowBlock[0].append(seat);
+            } else if (textId.includes('smallRowBlock_2block')) {
+               let seat = document.createElement("div")
+               seat.className = cl;
+               seat.id = textId;
+               smallRowBlock[1].append(seat);
+            }
+         }
+         chosenSeats = [];
          updatePrice();
-      } else if (seat.className === "seat-single chosen") {
-         seat.classList.remove("chosen")
-         seat.classList.add("available")
-         chosenSeats.pop(seat);
-         updatePrice();
-      } else if (seat.className === "seat-single unavailable") {
-         alert("This seat is already booked. Please choose another one")
+         chooseSeat();
+         console.log(`${chosenMovie}`, `${chosenCinema}`, `${chosenDate}` + `${chosenMonth}`, `${chosenTime}`);
       }
-   })
+   );
+}
+
+function chooseSeat() {
+   const seats = document.querySelectorAll('.seat-single')
+   console.log(seats);
+   for (let seat of seats) {
+      seat.addEventListener('click', () => {
+         if (seat.className === "seat-single available") {
+            seat.classList.remove("available")
+            seat.classList.add("chosen")
+            chosenSeats.push(seat);
+            updatePrice();
+         } else if (seat.className === "seat-single chosen") {
+            seat.classList.remove("chosen")
+            seat.classList.add("available")
+            chosenSeats.pop(seat);
+            updatePrice();
+         } else if (seat.className === "seat-single unavailable") {
+            alert("This seat is already booked. Please choose another one")
+         }
+         console.log(seat, chosenSeats);
+      })
+   }
 }
 
 
@@ -79,10 +155,8 @@ function updatePrice() {
    currentPrice.innerText = `${price} ₽`;
 }
 
-updatePrice();
 
-
-/*function occupyingSeats() {
+function occupyingSeats(seats) {
    for (let seat of seats) {
       if (seat.className === "seat-single chosen") {
          seat.classList.remove("chosen")
@@ -91,16 +165,41 @@ updatePrice();
    }
 }
 
-function submitToJson() {
-   LocalStorage.setItem("selectedMovie", chosenMovie);
-   LocalStorage.setItem("selectedCinema", chosenCinema);
-   LocalStorage.setItem("selectedMonth", chosenMonth);
-   LocalStorage.setItem("selectedDate", chosenDate);
-   LocalStorage.setItem("selectedTime", chosenTime);
-   LocalStorage.setItem("seats", JSON.stringify(seats));
+
+async function saveToFirestore() {
+   if (chosenSeats === false) return;
+   else {
+      console.log("firestore info:", `${chosenMovie}`, `${chosenCinema}`, `${chosenDate}` + `${chosenMonth}`, `${chosenTime}`, chosenSeats);
+      for (let seat of chosenSeats) {
+         await db.collection('movie').doc(`${chosenMovie}`).collection(`${chosenCinema}`).doc(`${chosenDate}` + `${chosenMonth}`).collection(`${chosenTime}`).doc(`${seat.id}`).set({
+            class: seat.className,
+            elemId: seat.id
+         })
+         console.log(seat + " saved");
+      }
+   }
 }
 
-buyBtn.addEventListener('click', () => {
-   occupyingSeats();
-   submitToJson();
-})*/
+
+
+//service function for cleaning seats layout from unavailables
+
+function clearSeats() {
+   const seats = document.querySelectorAll('.seat-single');
+   chosenSeats = [];
+   for (let seat of seats) {
+      if (seat.className === "seat-single chosen") {
+         seat.classList.remove("chosen");
+         seat.classList.add("available");
+         chosenSeats.push(seat);
+      } else if (seat.className === "seat-single unavailable") {
+         seat.classList.remove("unavailable");
+         seat.classList.add("available");
+         chosenSeats.push(seat);
+      }
+   }
+   saveToFirestore(seats).then(() => {
+      chosenSeats = [];
+      console.log("seats reloaded");
+   });
+}
